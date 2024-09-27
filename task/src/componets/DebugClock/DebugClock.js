31a97ca018f00872c9d418cbc6ee810ba8741e47
Nagender211 <dnagender2019@gmail.com>
2024-09-27 21:15:51 +0530

    Api routing Debugg Clock

--- task/src/componets/DebugClock/DebugClock.js
+++ task/src/componets/DebugClock/DebugClock.js
@@ -2,10 +2,85 @@ import React, { Component } from 'react'
 import Clock from './Clock'
 
 class DebugClock extends Component{
+    state={
+        count: 0,
+        min: 0
+       
+    }
+    timeCOunt=()=>{
+        
+       
+            this.setState((prevState)=>({
+                count: prevState.count+1
+            }))
+            if(this.state.count>59){
+                this.setState((prevState)=>({
+                    count: 0,
+                    min: prevState.min+1
+                }))
+            }
+        
+    }
+    toggelBtn=()=>{
+        if(!this.interval){
+            console.log("timmer is started")
+            this.interval=setInterval(() => {
+                this.timeCOunt()
+                console.log(this.state.count)
+                
+                
+            }, 1000);
+        }
+        // this.setState((prevState)=>({
+        //     count: prevState.count+1
+        // }))
+     
+    }
+    toggelBtnStop=()=>{
+        console.log("timer is stopped ")
+        if(this.interval){
+            clearInterval(this.interval);
+            this.interval = null;
+            
+        }
+        
+    }
+    restBtn=()=>{
+        this.setState({count: 0})
+        console.log("timer is reset")
+        clearInterval(this.interval);
+        this.interval = null;
+    }
+    
+    componentDidMount(){
+        
+        
+        this.interval=null
+
+    }
+   
+    componentWillUnmount(){
+        clearInterval(this.interval)
+
+    }
+    
+    
+    
+   
+    
     render(){
+        const {isStart,count,min} = this.state
+        
         return(
             <div>
-                <h1>DebugClock</h1>
+                {min}:{count<10 ? `0${count}`: `${count}`}
+                <br />
+
+                
+                
+                <button onClick={this.toggelBtn}>Start</button>
+                <button onClick={this.toggelBtnStop}>Stop</button>
+                <button onClick={this.restBtn}>Reset</button>
                 <Clock />
             </div>
         )
