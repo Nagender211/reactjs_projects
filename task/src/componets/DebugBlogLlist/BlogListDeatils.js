9ebc59b97c4c7d48993833c45bdcd8bf12ca7b61
Nagender211 <dnagender2019@gmail.com>
2024-09-29 15:15:31 +0530

    Api routing Debugg Blog

--- task/src/componets/DebugBlogLlist/BlogListDeatils.js
+++ task/src/componets/DebugBlogLlist/BlogListDeatils.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { useParams } from 'react-router-dom';
 
-const BlogListDeatils = () => {
-  return (
-    <div>
-        <h2>Blog List Details</h2>
-      
-    </div>
-  )
+class BlogListDeatils extends Component {
+  state = {
+    blogDetails: {}, 
+  };
+
+  componentDidMount() {
+    this.getIndividual();
+  }
+
+  getIndividual = async () => {
+    const {id}=this.props;
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`);
+    const data=await response.json();
+    const update={
+      author: data.author_name,
+      avatarUrl: data.avatar_url,
+      content: data.content,
+      imageUrl: data.image_url,
+      title: data.title,
+      topic: data.topic,
+    }
+    this.setState({ blogDetails: update });
+  };
+
+
+  render() {
+    const { blogDetails } = this.state;
+    const {author,avatarUrl,content,imageUrl,title,topic}=blogDetails
+    return (
+      <div>
+        <h1>{title}</h1>
+        <img src={imageUrl} alt="blog-image" />
+        <p>{content}</p>
+        <p>Author: {author}</p>
+        <p>Topic: {topic}</p>
+        <img src={avatarUrl} alt={avatarUrl} />
+       
+        
+      </div>
+    );
+  }
 }
 
-export default BlogListDeatils
+// To use with React Router, wrap it with a higher-order component to get the params
+const BlogDetailsWithParams = (props) => {
+  const { id } = useParams();
+  return <BlogListDeatils {...props} id={id} />;
+};
+
+export default BlogDetailsWithParams;
+
+// export default BlogListDeatils
--- task/src/componets/DebugBlogLlist/BlogLlist.js
+++ task/src/componets/DebugBlogLlist/BlogLlist.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const BlogLlist = (props) => {
     const {item}=props;
-    const {author,avatarUrl,imageUrl,title,topic}=item
+    const {author,avatarUrl,imageUrl,title,topic,id}=item
   return (
     <div>
-        <img src={avatarUrl} alt={author} />
+      <Link to={`blogs/${id}`}>
+      <img src={avatarUrl} alt={author} />
         <h2>{title}</h2>
         <p>Author: {author}</p>
         <p>Topic: {topic}</p>
         <img src={imageUrl} alt={title} />
+      </Link>
+       
         {/* <p>Read more: <a href={`https://www.example.com/blog/${title.toLowerCase().replace(' ', '-')}`}>Link</a></p> */}
   
       
