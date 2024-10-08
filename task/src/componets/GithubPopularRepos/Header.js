4a60e895a477481f583ffb03beaad721b5d95d02
Nagender211 <dnagender2019@gmail.com>
2024-10-08 13:31:14 +0530

    Github poppular

---
+++ task/src/componets/GithubPopularRepos/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <ul>
+            <li><Link to='/'>All</Link></li>
+            <li><Link to='/javaScript'>JavaScript</Link></li>
+            <li><Link to='/ruby'>Ruby</Link></li>
+            <li><Link to='/java'>Java</Link></li>
+            <li><Link to='/css'>CSS</Link></li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Header
