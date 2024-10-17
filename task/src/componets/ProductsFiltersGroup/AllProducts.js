b9226b66de6a383af88e5b671e812fe1e3f58e44
Nagender211 <dnagender2019@gmail.com>
2024-10-17 21:57:20 +0530

    ProductFillterGrooup

--- task/src/componets/ProductsFiltersGroup/AllProducts.js
+++ task/src/componets/ProductsFiltersGroup/AllProducts.js
@@ -2,19 +2,24 @@ import React from 'react'
 import Header from './Header'
 
 const AllProducts = (props) => {
-    const {each}=props;
-    const {id,name,price}=each;
+    const { each } = props;
+
+    // Add a check to ensure 'each' is defined before destructuring
+    if (!each) {
+        return null; // Or return some fallback UI
+    }
+
+    const { id, name, price } = each;
     
-  return (
-    <div>
-        <Header />
-       
-       <h1>{name}</h1>
-       <p>Price: ${price}</p>
-        
-      
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <h1>{name}</h1>
+            <p>Price: ${price}</p>
+        </div>
+    )
 }
 
 export default AllProducts
+
+// export default AllProducts
--- task/src/componets/ProductsFiltersGroup/Products.js
+++ task/src/componets/ProductsFiltersGroup/Products.js
@@ -1,41 +1,45 @@
-import React, { Component } from 'react'
-import Header from './Header'
-import AllProducts from './AllProducts'
+import React, { Component } from 'react';
+import Header from './Header';
+import AllProducts from './AllProducts';
 
-
-const productList=[{
-  id: 1,
-  name: 'Product 1',
-  price: 100
-}
-,
+const productList = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+  },
   {
     id: 2,
     name: 'Product 2',
-    price: 200
+    price: 200,
   },
   {
     id: 3,
     name: 'Product 3',
-    price: 300
-  }
+    price: 300,
+  },
+];
 
-]
+class Products extends Component {
+  state = {
+    products: productList,
+  };
 
-class Products extends Component{
-  state={
-    products: productList
-  }
   render() {
-    const {products}=this.state
+    const { products } = this.state;
     return (
       <div>
-        {products.map((each)=>(
-          <AllProducts each={each} key={each.id} />
+        <Header />
+        {products.map((each) => (
+          // Make sure to add the key prop here
+          <AllProducts key={each.id} each={each} />
         ))}
       </div>
-    )
+    );
   }
 }
 
-export default Products
+export default Products;
+
+
+// export default Products
